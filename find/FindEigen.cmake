641688fa332e8d1d396dc82157f6cd05e807dc09
a-price <arprice@gatech.edu>
2013-01-29 16:19:45 -0500

    Added project settings

---
+++ find/FindEigen.cmake
@@ -0,0 +1,2 @@
+# My hackish way of finding OpenCV, since there doesn't seem to be an existing Find.
+set(EIGEN3_INCLUDE_DIR "/usr/include/eigen3")
---
+++ find/FindLibfreenect.cmake
@@ -0,0 +1,2 @@
+# My hackish way of finding libfreenect, since there doesn't seem to be an existing Find.
+set(OpenCV_DIR "/usr/local/share/OpenCV")
