f63e54b67f580c5914e01773d7e3a88d874bbc57
Andrew Price <arprice@Fugue.(none)>
2013-02-15 08:53:53 -0800

    Documentation regenerated.

--- search/all_62.js
+++ search/all_62.js
@@ -2,6 +2,5 @@ var searchData=
 [
   ['bad_5fside',['BAD_SIDE',['../_hubo_joints_8h.html#ab883777acd7dd0c0f112ed1d190eb41ca9a3d354277e5206c62f7dbd23b6c3f87',1,'HuboJoints.h']]],
   ['blurradius',['blurRadius',['../struct_color_tracker_params.html#aa6561c1955484e1027e8ba82d6fd6f43',1,'ColorTrackerParams']]],
-  ['buffer_5flow_5fthreshold',['BUFFER_LOW_THRESHOLD',['../_ring_buffer_8h.html#ac616d24d05e3f38929699f70361b7991',1,'RingBuffer.h']]],
   ['buffermanager',['BufferManager',['../class_ring_buffer.html#afda391cac9df742dbc419c9730c5a8e4',1,'RingBuffer::BufferManager(void)'],['../class_ring_buffer.html#a56b6dbe231baf6e24065a68b7f740c5f',1,'RingBuffer::BufferManager(int size)']]]
 ];
